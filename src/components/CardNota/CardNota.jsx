74ddc4fe7d26d3703c68e39214a57e845476a61d
danielemaraschin <danikmaraschin@gmail.com>
2021-06-09 09:14:16 +0200

    import icon

--- src/components/CardNota/CardNota.jsx
+++ src/components/CardNota/CardNota.jsx
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
 import "./estilo.css";
+import deleteSVG from "../../assets/img/delete.svg"  //salvou o caminho dentro da var deleteSVG
+
 class CardNota extends Component {
   render() {
     return (
       <section className="card-nota">
         <header className="card-nota_cabecalho">
           <h3 className="card-nota_titulo">{this.props.titulo}</h3>
+          <img src={deleteSVG}  /*deleteSVG é uma var JS por isso entre chaves*/ />
         </header>
         <p className="card-nota_texto">{this.props.texto}</p>
       </section>
