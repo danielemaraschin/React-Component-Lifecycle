d73adb5f06ce50e98e0c4920328ac676eb65628b
danielemaraschin <danikmaraschin@gmail.com>
2021-06-10 23:06:31 +0200

    working with CSS

--- src/components/ListaCategorias/ListaDeCategorias.jsx
+++ src/components/ListaCategorias/ListaDeCategorias.jsx
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
-import "./estilo.css"
+import "./estilo.css";
 
 class  ListaDeCategorias extends Component {
     render() { 
         return (
             <section className="lista-categorias">
-                <ul>
-                    <li>Categorias</li>
-                    <li>Categorias</li>
-                    <li>Categorias</li>
-                    <li>Categorias</li>
-                    <li>Categorias</li>               
+                <ul className="lista-categorias_lista">
+                    <li className="lista-categorias_item">Categorias</li>
+                    <li className="lista-categorias_item">Categorias</li>
+                    <li className="lista-categorias_item">Categorias</li>
+                    <li className="lista-categorias_item">Categorias</li>
+                    <li className="lista-categorias_item">Categorias</li>               
                 </ul>
                 <input type= "text" className="lista-categorias_input"/>
             </section>
