56b87ac4ee4acb74071960a3fe6557f54596b839
danielemaraschin <danikmaraschin@gmail.com>
2021-06-12 22:04:47 +0200

    working with .map, props, ...

--- src/components/ListaCategorias/ListaDeCategorias.jsx
+++ src/components/ListaCategorias/ListaDeCategorias.jsx
@@ -9,16 +9,13 @@ class  ListaDeCategorias extends Component {
         }
     }
 
-
     render() { 
         return (
             <section className="lista-categorias">
                 <ul className="lista-categorias_lista">
-                    <li className="lista-categorias_item">Categorias</li>
-                    <li className="lista-categorias_item">Categorias</li>
-                    <li className="lista-categorias_item">Categorias</li>
-                    <li className="lista-categorias_item">Categorias</li>
-                    <li className="lista-categorias_item">Categorias</li>               
+                    {this.props.categorias.map((categoria, index) => {
+                        <li className="lista-categorias_item">Categorias</li> 
+                    })}              
                 </ul>
                 <input type= "text" 
                     className="lista-categorias_input" 
