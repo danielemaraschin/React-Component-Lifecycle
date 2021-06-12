12c4270d2ee678d228ab656ad8d29d43f00268ee
danielemaraschin <danikmaraschin@gmail.com>
2021-06-12 22:16:14 +0200

    Working with target.value added on input

--- src/components/ListaCategorias/ListaDeCategorias.jsx
+++ src/components/ListaCategorias/ListaDeCategorias.jsx
@@ -2,9 +2,10 @@ import React, { Component } from 'react';
 import "./estilo.css";
 
 class ListaDeCategorias extends Component {
-    _handleEventoInput(e) {
+    _handleEventoInput(e) { //"e" é o evento
         console.log(e.key)
         if (e.key == "Enter") {
+            let valorCategoria =e.target.value //target value vai pegar o valor do evento que eh o valor add no input
            this.props.adicionarCategoria(valorCategoria);
         }
     }
