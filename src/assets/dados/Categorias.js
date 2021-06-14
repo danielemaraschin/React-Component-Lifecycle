6b9a2735239af09b5ea9a9ab78caab7c847a7f1b
danielemaraschin <danikmaraschin@gmail.com>
2021-06-14 11:59:26 +0200

    adding new information on arra with .push

--- src/assets/dados/Categorias.js
+++ src/assets/dados/Categorias.js
@@ -2,4 +2,9 @@ export default class Categorias{
         constructor(){
             this.categorias = []
         }
+
+adicionarCategoria(novaCategoria){
+
+    this.categoria.push(novaCategoria) //add no array a nova categoria
+  }
 }
