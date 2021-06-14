b913a5f51b48dd4a8274f056b798a7697281b684
danielemaraschin <danikmaraschin@gmail.com>
2021-06-14 12:08:35 +0200

    creating new classes

--- src/assets/dados/Notas.js
+++ src/assets/dados/Notas.js
@@ -2,4 +2,18 @@ export default class ArrayDeNotas{
     constructor(){
         this.notas = []
     }
+
+    adicionarNota(titulo, texto, categoria){
+        const novaNota = new Nota(titulo, texto, categoria)
+        this.notas.push()
+
+    }
+}
+
+class Nota{
+    constructor(titulo, texto, categoria){
+        this.titulo = titulo;
+        this.texto = texto;
+        this.categoria = categoria;
+    }
 }
