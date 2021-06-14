1dcb4917918d538ca58799ed20504b20345745a0
danielemaraschin <danikmaraschin@gmail.com>
2021-06-14 12:11:21 +0200

    deleting app state

--- src/App.js
+++ src/App.js
@@ -11,32 +11,7 @@ class App extends Component {
   constructor(){
     super();
 
-    this.state = {
-      notas:[],
- //     categorias:[]
-    };
-  }
-
- /* criarNota(titulo, texto, categoria){
-    const novaNota = {titulo, texto, categoria};
-    const novoArrayNotas = [...this.state.notas,novaNota]
-    const novoEstado = {notas:novoArrayNotas}
-    this.setState(novoEstado)
-  }
-
-  adicionarCategoria(nomeCategoria){
-    const novoArrayCategorias = [...this.state.categorias,nomeCategoria]
-    const novoEstado = {...this.state, categorias:novoArrayCategorias};
-    this.setState(novoEstado)
-  }*/
-
-  deletarNota(index){
-    let arrayNotas = this.state.notas
-    arrayNotas.splice(index,1)
-    this.setState({notas: arrayNotas})
-  }
-
-  render() {
+    render() {
     return (
       <section className="conteudo">
         <FormularioCadastro 
