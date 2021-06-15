68abedc06672af96868261a07f7c6884114cfdba
danielemaraschin <danikmaraschin@gmail.com>
2021-06-15 22:27:01 +0200

    Learning about observable

--- src/assets/dados/Categorias.js
+++ src/assets/dados/Categorias.js
@@ -1,18 +1,20 @@
-export default class Categorias{
+export default class Categorias {
     constructor() {
         this.categorias = [];
         this._inscritos = [];
     }
 
-    inscrever(func){
+    inscrever(func) {
         this._inscritos.push(func);
     }
 
-    notificar(){
-        this._inscritos.forEach(func => func ()); //recebe como parametro a funcao 'func'que vai executar a func()
+    notificar() {
+        this._inscritos.forEach(func => {
+            func(); //recebe como parametro a funcao 'func'que vai executar a func()
+        });
     }
 
-    adicionarCategoria(novaCategoria){
+    adicionarCategoria(novaCategoria) {
         console.log(this.categorias);
         this.categorias.push(novaCategoria); //add no array a nova categoria pelo push
     }
