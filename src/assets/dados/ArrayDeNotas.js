317ccd0096d9e8f3e2b3596d2d2a5829452f60af
danielemaraschin <danikmaraschin@gmail.com>
2021-06-15 22:31:21 +0200

    Learning about observable

--- src/assets/dados/ArrayDeNotas.js
+++ src/assets/dados/ArrayDeNotas.js
@@ -1,7 +1,19 @@
 export default class ArrayDeNotas{
     constructor(){
         this.notas = [];
+        this._inscritos = [];
     }
+
+    inscrever(func) {
+        this._inscritos.push(func);
+    }
+
+    notificar() {
+        this._inscritos.forEach(func => { //recebe como parametro a funcao 'func'que vai executar a func()
+            func(this.categorias); //pega as novas informacoes da fonte de dados do array de categorias e notifica os inscritos
+        });
+    }
+
 
     adicionarNota(titulo, texto, categoria){
         const novaNota = new Nota(titulo, texto, categoria);
