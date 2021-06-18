c4258f83e9a47120fd69fa5fd1cf6350af00c1cd
danielemaraschin <danikmaraschin@gmail.com>
2021-06-18 20:59:26 +0200

    fixing the code

--- src/components/ListaCategorias/ListaDeCategorias.jsx
+++ src/components/ListaCategorias/ListaDeCategorias.jsx
@@ -33,7 +33,11 @@ class ListaDeCategorias extends Component {
             <section className="lista-categorias">
                 <ul className="lista-categorias_lista">
                     {this.state.categorias.map((categoria, index) => {
-                        return <li key={index} className="lista-categorias_item">{categoria}</li>
+                        return (
+                            <li key={index} className="lista-categorias_item">
+                                {categoria}
+                            </li>
+                        );
                     })}
                 </ul>
                 <input type="text"
