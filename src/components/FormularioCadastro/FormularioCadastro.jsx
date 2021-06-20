bd2827ef41d11dee46ebddb6ea86ccab189467a9
danielemaraschin <danikmaraschin@gmail.com>
2021-06-21 00:32:45 +0200

    fixing the code

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,64 +1,60 @@
+  
 import React, { Component } from "react";
 import "./estilo.css";
 class FormularioCadastro extends Component {
-
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.titulo ="";
-    this.texto ="";
-    this.categoria = " ";
-    this.state = {categorias:[]};
+    this.titulo = "";
+    this.texto = "";
+    this.categoria = "Sem Categoria";
+    this.state = {categorias:[]}
 
     this._novasCategorias = this._novasCategorias.bind(this);
   }
+
   componentDidMount(){
     //aqui o bind recebe uma funcao como parametro e retorna uma nova funcao como parametro
-    this.props.categorias.inscrever(this._novasCategorias);//inscreve o formulario p receber as novas categorias
+    this.props.categorias.inscrever( this._novasCategorias);//inscreve o formulario p receber as novas categorias
    
   }  
   componentWillUnmount(){
-    this.props.categorias.desinscrever(this._novasCategorias);
+    this.props.categorias.desinscrever( this._novasCategorias);
   }
- 
-
   _novasCategorias(categorias){
     this.setState({...this.state, categorias})
   }
-
-  _handleMudancaTitulo(evento){
+  _handleMudancaCategoria(evento){
+    evento.stopPropagation();
+    this.categoria = evento.target.value;
+  }
+  _handleMudancaTitulo(evento) {
     evento.stopPropagation();
     this.titulo = evento.target.value;
   }
 
-  _handleMudancaCategoria(evento){
+  _handleMudancaTexto(evento) {
     evento.stopPropagation();
     this.texto = evento.target.value;
   }
 
-  _handleMudancaTexto(evento){
-    evento.stopPropagation();
-    this.texto = evento.target.value;
-  }
-
-  _criarNota(evento){
+  _criarNota(evento) {
     evento.preventDefault();
     evento.stopPropagation();
     this.props.criarNota(this.titulo, this.texto, this.categoria);
-    
   }
 
   render() {
     return (
       <form className="form-cadastro" onSubmit={this._criarNota.bind(this)}>
-        <select 
+        <select
           onChange={this._handleMudancaCategoria.bind(this)}
           className="form-cadastro_input"
         >
-         <option> Sem Categoria</option>
+          <option>Sem Categoria</option>
 
-        {this.state.categorias.map((categoria, index) =>{
-          return <option key={index} >{categoria}</option>;
-        })}
+          {this.state.categorias.map((categoria, index) => {
+            return <option key={index} >{categoria}</option>;
+          })}
         </select>
         <input
           type="text"
